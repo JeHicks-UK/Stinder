e0abf2dbe2ead5202d10b5122c311a038c93b77e
karlkerem <karl.kerem@gmail.com>
2016-02-22 01:24:37 +0000

    Matches page main implementation

--- web/app/scripts/controllers/matches.js
+++ web/app/scripts/controllers/matches.js
@@ -5,18 +5,31 @@ angular.module('hackAppApp')
   .controller('MatchesCtrl', function (matchService, uiGridConstants) {
 
     var scope = this;
-    matchService.getYourMatches(function(matches) {
-      console.log(matches);
-      scope.matches = matches;
-      showMatchDetails(0);
-    });
+
+    scope.gameGridOptions = {
+      enableColumnMenus: false,
+      rowHeight: 69,
+      enableSorting: true,
+      showSelectionCheckbox: false,
+      enableFullRowSelection: false,
+      enableRowSelection: false,
+      multiSelect: false,
+      enableHorizontalScrollbar: false,
+      onRegisterApi: function(gridApi){
+        scope.gridApi = gridApi;
+      }
+    };
 
 
-    scope.gameGridOptions = {};
+    this.showMatchDetails = function(index) {
 
-    var showMatchDetails = function(index) {
+      scope.matches.forEach(function(match) {
+        match.selected = false;
+      });
+      scope.matches[index].selected = true;
 
       var match = scope.matches[index]._user;
+      scope.matchDetails = match;
       var count = 0;
       match.ownedGames = match.ownedGames.filter(function(game) {
         if(count<5) {++count; return true;}
@@ -28,36 +41,45 @@ angular.module('hackAppApp')
       });
       console.log(match);
 
+      scope.gameGridOptions.minRowsToShow = match.ownedGames.length>4 ? 4 : match.ownedGames.length;
 
-      scope.gameGridOptions = {
-        enableColumnMenus: false,
-        rowHeight: 69,
-        minRowsToShow: match.ownedGames.length>4 ? 4 : match.ownedGames.length,
-        enableSorting: true,
-        showSelectionCheckbox: false,
-        enableFullRowSelection: false,
-        enableRowSelection: false,
-        multiSelect: false,
-        enableHorizontalScrollbar: false,
-        enableVerticalScrollbar: match.ownedGames.length>4 ? uiGridConstants.scrollbars.ALWAYS : uiGridConstants.scrollbars.NEVER,
-        columnDefs: [
-          { field: 'image', cellTemplate: "<img src=\"{{row.entity.imageUrl}}\" lazy-src>", maxWidth: 184, enableSorting: false},
-          { field: 'name' },
-          { field: 'playtime_forever',
-            displayName: 'Playtime',
-            maxWidth: 90,
-            sort: {
-              direction: uiGridConstants.DESC,
-              priority: 0
-            },
-            cellTemplate: "<span class='playtime-cell-content'>{{row.entity.playtime_forever/60 | number : 0}} h</span>"
-          }
+      scope.gameGridOptions.enableVerticalScrollbar = match.ownedGames.length>4 ? uiGridConstants.scrollbars.ALWAYS : uiGridConstants.scrollbars.NEVER;
+      scope.gameGridOptions.columnDefs = [
+        { field: 'image', cellTemplate: "<img src=\"{{row.entity.imageUrl}}\" lazy-src>", maxWidth: 184, enableSorting: false},
+        { field: 'name' },
+        { field: 'playtime_forever',
+          displayName: 'Playtime',
+          maxWidth: 90,
+          sort: {
+            direction: uiGridConstants.DESC,
+            priority: 0
+          },
+          cellTemplate: "<span class='playtime-cell-content'>{{row.entity.playtime_forever/60 | number : 0}} h</span>"
+        }
 
-        ],
-        data: match.ownedGames
-      };
+      ];
+      scope.gameGridOptions.data = match.ownedGames;
+
+      if(scope.gridApi) {
+        scope.gridApi.core.refresh();
+      }
 
     };
+
+
+
+    matchService.getYourMatches(function(matches) {
+      console.log(matches);
+      if(matches.length && matches.length>0) {
+        scope.matches = matches;
+        scope.showMatchDetails(0);
+      }
+    });
+
+
+
+
+
 
 
 
